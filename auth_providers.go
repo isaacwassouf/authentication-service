69a7f83ec5074bfb40d91c4d45587bafba3fc2ca
Isaac Wassouf <isaacwassouf@gmail.com>
2024-06-11 14:57:08 +0200

    feat: add rpc method for getting google authorization url; update submodule

--- auth_providers.go
+++ auth_providers.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"net/url"
+	"os"
 	"time"
 
 	sq "github.com/Masterminds/squirrel"
+	"github.com/matoous/go-nanoid/v2"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 
 	pb "github.com/isaacwassouf/authentication-service/protobufs/users_management_service"
 )
@@ -116,4 +121,60 @@ func (s *UserManagementService) DisableAuthProvider(
 	}
 
 	return &pb.DisableAuthProviderResponse{Message: "Auth provider enabled successfully"}, nil
+}
+
+func (s *UserManagementService) GetGoogleAuthorizationUrl(
+	ctx context.Context,
+	in *emptypb.Empty,
+) (*pb.GoogleAuthorizationUrlResponse, error) {
+	// set the base url for the google authorization url
+	baseURL, err := url.ParseRequestURI("https://accounts.google.com/o/oauth2/v2/auth")
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Failed to parse the base url")
+	}
+
+	var clientId string
+	var active bool
+	// get the client_id and active fields from the auth_providers_details table
+	query := sq.Select("auth_providers_details.client_id", "auth_providers_details.active").
+		From("auth_providers").
+		Join("auth_providers_details ON auth_providers.id = auth_providers_details.auth_provider_id").
+		Where(sq.Eq{"auth_providers.name": "google"})
+
+	err = query.RunWith(s.userManagementServiceDB.db).QueryRow().Scan(&clientId, &active)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Error(codes.NotFound, "Auth provider not found")
+		}
+		return nil, status.Error(codes.Internal, "Failed to query the database")
+	}
+
+	// check if the auth provider is active
+	if !active {
+		return nil, status.Error(codes.PermissionDenied, "Auth provider is not active")
+	}
+
+	params := url.Values{}
+	params.Add("client_id", clientId)
+	params.Add("response_type", "code")
+	params.Add("scope", "openid email profile")
+
+	// generate a random state
+	state, err := gonanoid.New()
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Failed to generate a random state")
+	}
+	params.Add("state", state)
+
+	// get the redirect_uri from the environment variables
+	redirect_uri := os.Getenv("API_GATEWAY_GOOGLE_AUTHORIZATION_URL")
+	// if the redirect_uri is not set, use the default value
+	if redirect_uri == "" {
+		redirect_uri = "http://localhost:5173/api/auth/google/callback"
+	}
+	params.Add("redirect_uri", redirect_uri)
+
+	baseURL.RawQuery = params.Encode()
+
+	return &pb.GoogleAuthorizationUrlResponse{Url: baseURL.String(), State: state}, nil
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	github.com/matoous/go-nanoid/v2 v2.1.0 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
